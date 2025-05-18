d9c10cdca2f375cafd7bf5c8757ad7a103f891b8
alperen-yazici <212114717+AlperenYaziciWork@users.noreply.github.com>
2025-05-18 17:00:28 +0300

    ACS724 current sensor device driver module was implemented.

---
+++ saykal_buck_converter/Project_Configs/ACS724_CS_cfg/ACS724_CS_cfg.h
@@ -0,0 +1,15 @@
+/*
+ * ACS724_CS_cfg.h
+ *
+ *  Created on: May 18, 2025
+ *      Author: alperenyazici
+ */
+
+#ifndef ACS724_CS_CFG_ACS724_CS_CFG_H_
+#define ACS724_CS_CFG_ACS724_CS_CFG_H_
+
+// it represents ACS724 sensor id in configuration structure in ACS724_CS_cfg.c file
+#define BUCK_CONVERTOR_OUT_ACS724_CS_ID		0U
+
+#define TOTAL_ACS724_CS						1U
+#endif /* ACS724_CS_CFG_ACS724_CS_CFG_H_ */
