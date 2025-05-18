a8fff3c6ee2ec20e0795bf3a370bb7acbd1c622f
alperen-yazici <212114717+AlperenYaziciWork@users.noreply.github.com>
2025-05-18 18:33:48 +0300

    Comment lines were added to adc sensor driver.

--- saykal_buck_converter/Source/device_drivers/adc_sensor_driver/adc_sensor_driver.c
+++ saykal_buck_converter/Source/device_drivers/adc_sensor_driver/adc_sensor_driver.c
@@ -2,8 +2,23 @@
 
 #include "adc_sensor_driver.h"
 
+/**
+ * @brief Pointer to the configuration array holding all ADC sensors parameters.
+ */
 static const adc_sensor_driver_config_t *m_adc_sensor_driver_configs_ptr = NULL;
 
+/**
+ * @brief Initializes the ADC sensor driver with the provided configuration.
+ * 
+ * This function stores the pointer to the array of ADC sensor configurations
+ * that contain the necessary calibration and function references to read and convert
+ * sensor values.
+ * 
+ * @param[in] adc_sensors_config Pointer to an array of ADC sensor driver configuration structures.
+ *                               Must not be NULL.
+ * 
+ * @retval None
+ */
 void init_adc_sensor_driver(const adc_sensor_driver_config_t *adc_sensors_config)
 {
 	if(NULL == adc_sensors_config)
@@ -15,6 +30,24 @@ void init_adc_sensor_driver(const adc_sensor_driver_config_t *adc_sensors_config
 	m_adc_sensor_driver_configs_ptr = adc_sensors_config;
 }
 
+/**
+ * @brief Reads and converts the ADC sensor value for a given sensor ID.
+ * 
+ * This function uses the function pointer defined in the configuration to obtain the raw voltage
+ * value from the hardware ADC, then applies scaling and offset to convert it into a physical
+ * sensor value (e.g., temperature, current, etc.).
+ * 
+ * The conversion is done using the following formula:
+ * @code
+ *     sensor_value = ((raw_voltage * raw_voltage_factor) - reference_voltage) / sensitivity
+ * @endcode
+ * 
+ * @param[in]  sensor_id         Index of the sensor in the configuration array (used as a unique ID).
+ * @param[out] sensor_value_ptr Pointer to a float variable where the calculated sensor value will be stored.
+ *                               Must not be NULL.
+ * 
+ * @retval None
+ */
 void read_adc_sensor_value(uint8_t sensor_id , float *sensor_value_ptr)
 {
 	float read_data_for_adc_raw_voltage = 0.0f;
