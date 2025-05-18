b13437b4dce88b42b8906a21b2591ad19c4e1031
alperen-yazici <212114717+AlperenYaziciWork@users.noreply.github.com>
2025-05-18 19:14:09 +0300

    library of PID is implemented.

---
+++ saykal_buck_converter/Source/libraries/pid_controller/pid_controller.c
@@ -0,0 +1,51 @@
+
+#include "pid_controller.h"
+
+float PID_Step(pid_config_t *pid_parameters_ptr, float sensed_value, float reference_point)
+{
+
+    float calculated_error;
+    float command;
+    float command_sat;
+    float derivation_measure;
+
+    /* Error calculation */
+    calculated_error = reference_point - sensed_value;
+
+    /* Integral term calculation - including anti-windup */
+    pid_parameters_ptr->integral += 
+        pid_parameters_ptr->Ki*calculated_error*pid_parameters_ptr->TimeStep + 
+        pid_parameters_ptr->Kaw*
+        (pid_parameters_ptr->command_sat_prev - pid_parameters_ptr->command_prev)*
+        pid_parameters_ptr->TimeStep;
+    
+    /* Derivative term calculation using filtered derivative method */
+    derivation_measure = (calculated_error - pid_parameters_ptr->error_previous )/(pid_parameters_ptr->TimeStep);
+    pid_parameters_ptr->error_previous = calculated_error;
+
+    /* Summing the 3 terms */
+    command = pid_parameters_ptr->Kp*calculated_error + pid_parameters_ptr->integral + pid_parameters_ptr->Kd*derivation_measure;
+
+    /* Remember command at previous step */
+    pid_parameters_ptr->command_prev = command;
+
+    /* Saturate command */
+    if (command > pid_parameters_ptr->controller_output_max)
+    {
+        command_sat = pid_parameters_ptr->controller_output_max;
+    }
+    else if (command < pid_parameters_ptr->controller_output_min)
+    {
+        command_sat = pid_parameters_ptr->controller_output_min;
+    }
+    else
+    {
+        command_sat = command;
+    }
+
+    /* Remember saturated command at previous step */
+    pid_parameters_ptr->command_sat_prev = command_sat;
+
+    return command_sat;
+}
+
---
+++ saykal_buck_converter/Source/libraries/pid_controller/pid_controller.h
@@ -0,0 +1,30 @@
+/*
+ * pid_controller.h
+ *
+ *  Created on: May 18, 2025
+ *      Author: alperenyazici
+ */
+
+#ifndef PID_CONTROLLER_PID_CONTROLLER_H_
+#define PID_CONTROLLER_PID_CONTROLLER_H_
+
+typedef struct
+{
+    float Kp;              // Proportional gain constant
+    float Ki;              // Integral gain constant
+    float Kd;              // Derivative gain constant
+    float Kaw;             // Anti-windup gain constant
+    float TimeStep;               // Time step
+    float controller_output_max;             // Max controller_output
+    float controller_output_min;             // Min controller_output
+    float integral;        // Integral term
+    float error_previous;        // Previous error
+    float command_sat_prev;// Previous saturated controller_output
+    float command_prev;    // Previous controller_output
+
+}pid_config_t;
+
+
+float PID_Step(pid_config_t *pid_parameters_ptr, float sensed_value, float reference_point);
+
+#endif /* PID_CONTROLLER_PID_CONTROLLER_H_ */
