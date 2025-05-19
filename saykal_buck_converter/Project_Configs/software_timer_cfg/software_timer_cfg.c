74d1ba07003235f2d59b1b52c79648bfd1c10611
alperen-yazici <212114717+AlperenYaziciWork@users.noreply.github.com>
2025-05-19 13:48:11 +0300

    Updated software timer configuration because communication timer is added.

--- saykal_buck_converter/Project_Configs/software_timer_cfg/software_timer_cfg.c
+++ saykal_buck_converter/Project_Configs/software_timer_cfg/software_timer_cfg.c
@@ -8,13 +8,18 @@
 #include "software_timer.h"
 #include "stm32f4xx_hal.h"
 #include "app_buck_converter.h"
+#include "com_driver.h"
 
 const software_timer_cfg_t software_timers_configs[] =
 {
 	{
 		.reload_option = TIMER_RELOAD_PERIODIC,
 		.timeout_callback_func = control_out_voltage_with_current_limit,
-	}
+	},
+	{
+		.reload_option = TIMER_RELOAD_PERIODIC,
+		.timeout_callback_func = send_periodic_message_timeout_cb,
+	},
 };
 
 const software_timer_general_cfg_t g_software_timer_general_config =
